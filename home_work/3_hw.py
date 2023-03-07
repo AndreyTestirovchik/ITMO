594f258372656e8739d86feeab024407ac6ab4e3
andrey.testirovchik@gmail.com <andrey.testirovchik@gmail.com>
2023-03-07 19:28:06 +0300

    home_work_3_commit

---
+++ home_work/3_hw.py
@@ -0,0 +1,64 @@
+def add_1(a: int, b: int) -> int:
+    return max(a, b)
+
+
+print(max(15, 100))
+
+
+def add_2(a: int, b: int) -> str:
+    if a - b == 135:
+        result = 'Yes'
+    else:
+        result = 'No'
+    return result
+
+
+print(add_2(135, 5))
+
+
+def add_3(a: int) -> str:
+    if a in range(1, 3):
+        sizon = 'Зима'
+    elif a == 12:
+        sizon = 'Зима'
+    elif a in range(3, 6):
+        sizon = 'Весна'
+    elif a in range(6, 9):
+        sizon = 'Лето'
+    elif a in range(9, 12):
+        sizon = 'Осень'
+    else:
+        sizon = 'Введите число от 1 до 12 включительно'
+    return sizon
+
+
+print(add_3(3))
+
+
+def add_4(a: int, b: int, c: int) -> str:
+    if a > 10 and b > 10 and c > 10:
+        result = 'Yes'
+    else:
+        result = 'No'
+    return result
+
+
+print(add_4(11, 11, 11))
+
+
+def add_5(a: list) -> int:
+    result = 0
+    for elem in a:
+        if elem >= 0:
+            result += 1
+    return result
+
+
+print(add_5((1, 2, -3, -4, -5)))
+
+
+def add_6(years: int, months: int) -> int:
+    return 29 * months + 29 * 12 * years
+
+
+print(add_6(6, 8))
