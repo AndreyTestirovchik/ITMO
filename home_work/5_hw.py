91695042c0b600d46daa1756dfdf8e888e47aeb0
andrey.testirovchik@gmail.com <andrey.testirovchik@gmail.com>
2023-03-15 14:54:00 +0300

    home_work_5_update

--- home_work/5_hw.py
+++ home_work/5_hw.py
@@ -8,9 +8,13 @@ username = driver.find_element(By.CSS_SELECTOR, '#user-name')
 password = driver.find_element(By.CSS_SELECTOR, '#password')
 submit = driver.find_element(By.CSS_SELECTOR, '#login-button')
 
-if None in [username, password, submit]:
-    print('Элемент/Элементы не найдены')
-else:
-    print('Элементы найдены')
+
+def test_page(a, b, c):
+    if None in [a, b, c]:
+        text = 'Элемент/Элементы не найдены'
+    else:
+        text = 'Элементы найдены'
+    return text
 
 
+print(test_page(username, password, submit))
